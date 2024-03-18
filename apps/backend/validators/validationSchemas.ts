cdd4d9c402a88be738fa727041816a0a5232b100
Juan Posso <juan.posso@epn.edu.ec>
2024-03-18 13:15:57 -0500

    feat: Add validation schema for user registration

---
+++ apps/backend/validators/validationSchemas.ts
@@ -0,0 +1,9 @@
+import Joi from 'joi';
+
+export const registerSchema = Joi.object({
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  confirmPassword: Joi.ref('password'),
+});
