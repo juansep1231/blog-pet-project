0c9150a9ef92d54dfe89aa6e5057ebe8e59ad386
Juan Posso <juan.posso@epn.edu.ec>
2024-03-20 08:10:19 -0500

    chore: relocate validation schema

--- apps/backend/validators/validationSchemas.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-export const registerSchema = Joi.object({
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  confirmPassword: Joi.ref('password'),
-});
