ac478352c91128ed7d1e6fc4f68ce2cce56ca5b8
Juan Posso <juan.posso@epn.edu.ec>
2024-03-21 13:36:59 -0500

    feat: add express.json and cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-
+import registerRouter from './routers/Register';
+import cors from 'cors';
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
