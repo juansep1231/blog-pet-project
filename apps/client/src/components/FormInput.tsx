c811d8fa6ade0f61501177b12cb09fbad5e677e5
Juan Posso <juan.posso@epn.edu.ec>
2024-03-21 12:08:02 -0500

    feat: add helper text for errors

--- apps/client/src/components/FormInput.tsx
+++ apps/client/src/components/FormInput.tsx
@@ -11,6 +11,7 @@ interface FormInputProps {
   placeholder?: string;
   required?: boolean;
   helperText?: string;
+  error?: boolean;
 }
 
 const FormInput: React.FC<FormInputProps> = ({
@@ -21,10 +22,11 @@ const FormInput: React.FC<FormInputProps> = ({
   onChange,
   placeholder,
   required = false,
+  helperText,
+  error = false,
 }) => {
   return (
     <div className="">
-      <label htmlFor={name}>{label}</label>
       <div>
         <TextField
           required={required}
@@ -35,6 +37,8 @@ const FormInput: React.FC<FormInputProps> = ({
           value={value}
           onChange={onChange}
           placeholder={placeholder}
+          helperText={helperText}
+          error={error}
         ></TextField>
       </div>
     </div>
