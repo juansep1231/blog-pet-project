061726885c94335c6d6ccb2efddd2bec68dc3d67
Juan Posso <juan.posso@epn.edu.ec>
2024-04-02 08:51:28 -0500

    refactor: Update validation schemas for register form

--- apps/backend/src/validators/validationSchemas.ts
+++ apps/backend/src/validators/validationSchemas.ts
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 
 export const registerSchema = Joi.object({
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-  email: Joi.string().email().required(),
+  firstName: Joi.string().max(30).required(),
+  lastName: Joi.string().max(30).required(),
+  email: Joi.string().email().max(255).required(),
   password: Joi.string().min(6).required(),
   confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
 });
