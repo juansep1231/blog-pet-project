1d4e5e7c789470220ad9eca000ea35a70d4472f8
Juan Posso <juan.posso@epn.edu.ec>
2024-04-16 09:56:16 -0500

    refactor: update router import

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import registerRouter from './routers/Register';
+import { router } from './routers/Register';
 import cors from 'cors';
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('api/v1/register', registerRouter);
+app.use('/api/v1/register', router);
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
